b137b1d11c15e10c012f4f0aee6507ae258d421b
odenktools <odenktools86@gmail.com>
2018-02-25 16:03:16 +0700

    Clean Code #1

--- app/controllers/AdminController.java
+++ app/controllers/AdminController.java
@@ -1,27 +1,10 @@
 package controllers;
 
-import com.avaje.ebean.Ebean;
-import com.avaje.ebean.Query;
-import com.avaje.ebean.RawSql;
-import com.avaje.ebean.RawSqlBuilder;
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.wordnik.swagger.annotations.*;
-import play.libs.Json;
 
 import play.mvc.Result;
 import play.mvc.Security;
 import play.mvc.Controller;
 import play.mvc.Http;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.LinkedHashMap;
-import java.util.List;
-
 
 public class AdminController extends Controller {
 
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,10 +1,10 @@
 // The Typesafe repository
+logLevel := Level.Warn
+
+// The Typesafe repository
 resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
 
 // Use the Play sbt plugin for Play projects
 addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.3.10")
-
-// Plugin for eclipse development
-addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "3.0.0")
 
 addSbtPlugin("se.marcuslonnberg" % "sbt-docker" % "1.4.0")
